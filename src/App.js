ac90c3015eb63b0188a873292a2d0409befa9880
Obi Nwokogba <obi.nwokogba@gmail.com>
2024-08-23 13:15:26 -0400

    Develop UI and basic logic

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import { useState, useEffect, createContext, useContext } from "react";
 
 import "./styles.css";
 import BigTextDisplay from "./BigTextDisplay";
-import Shades from "./Shades";
 import ColorSampleCircle from "./ColorSampleCircle";
 import Footer from "./Footer";
+import RandomColors from "./RandomColors";
+import Shades from "./Shades";
 import Variants from "./Variants";
+
+export const CurrentColorContext = createContext(null);
 
 function App() {
   const [currentColorHex, setCurrentColorHex] = useState("#1E7BF3");
@@ -15,6 +18,10 @@ function App() {
   let [BValue, setBValue] = useState(127);
 
   let [HSLValueArray, setHSLValue] = useState([1, 1, 1]);
+
+  let setNewColor = (newColorHex) => {
+    console.log(`${newColorHex} - set New Color Called!`);
+  };
 
   function showNotification(message) {
     // TODO : Set Notification System
@@ -26,42 +33,47 @@ function App() {
   return (
     <>
       <div className="appContainer">
-        <span className="appTitle" style={{ color: currentColorHex }}>
-          coloressa 2 <span className="lighter2 smaller"></span>
-        </span>
+        <CurrentColorContext.Provider
+          value={{ currentColorHex, setCurrentColorHex }}
+        >
+          <span className="appTitle" style={{ color: currentColorHex }}>
+            color22 <span className="lighter2 smaller"></span>
+          </span>
 
-        <div className="displayColorContainer">
-          <ColorSampleCircle
-            currentColorHex={currentColorHex}
-            onClick={() => {
-              showNotification("Copied to Cluppy");
-            }}
-          />
-
-          <div className="colorDetailsColumn">
-            <BigTextDisplay currentColorHex={currentColorHex} />
-            <BigTextDisplay
-              currentColorHex={`rgb(${RValue}, ${GValue}, ${BValue})`}
+          <div className="displayColorContainer">
+            <ColorSampleCircle
+              currentColorHex={currentColorHex}
+              onClick={() => {
+                showNotification("Copied to Cluppy");
+              }}
             />
-            <BigTextDisplay
-              currentColorHex={`hsl(${HSLValueArray[0]}, ${HSLValueArray[1]}%, ${HSLValueArray[2]}%)`}
+
+            <div className="colorDetailsColumn">
+              <BigTextDisplay currentColorHex={currentColorHex} />
+              <BigTextDisplay
+                currentColorHex={`rgb(${RValue}, ${GValue}, ${BValue})`}
+              />
+              <BigTextDisplay
+                currentColorHex={`hsl(${HSLValueArray[0]}, ${HSLValueArray[1]}%, ${HSLValueArray[2]}%)`}
+              />
+            </div>
+
+            <Shades
+              func={setNewColor}
+              currentColorHSLArray={HSLValueArray}
+              currentColorHex={currentColorHex}
+            />
+
+            <Variants
+              currentColorHSLArray={HSLValueArray}
+              currentColorHex={currentColorHex}
             />
           </div>
 
-          <Shades
-            func={setNewColor}
-            currentColorHSLArray={HSLValueArray}
-            currentColorHex={currentColorHex}
-          />
-
-          <Variants
-            currentColorHSLArray={HSLValueArray}
-            currentColorHex={currentColorHex}
-          />
-        </div>
-
-        <div className="RGBSliderContainer"></div>
-
+          <div className="RGBSliderContainer">
+            <RandomColors />
+          </div>
+        </CurrentColorContext.Provider>
         <Footer />
       </div>
     </>
